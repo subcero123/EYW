4170ddc26047714bc0c20d06af79405c2a6a443c
subcero123 <subcero139@hotmail.com>
2024-01-25 21:16:52 -0600

    HOTELS added

--- index.php
+++ index.php
@@ -30,10 +30,13 @@
     <li>
       <div class="m-slider-item">
         <div class="m-slider-overlay">
-			<div class="corner-icons">
-				<div class="circle-icon">5&#9733</div>
+		<div class="corner-icons">
+				<div class="circle-icon">5<b style="color: yellow;">&#9733</b></div>
   			</div>
           	<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/logo.jpg" alt="Slider Image">
+			<div class="corner-container">
+    			<div class="circle-container"><b style="color: yellow;">$</b> 450</div>
+  			</div>
 			<div class="m-slider-text">
 				<div class="column">
 					<p class="bold-text">QUAD BIKE RIDES</p>
@@ -52,7 +55,13 @@
 	<li>
       <div class="m-slider-item">
         <div class="m-slider-overlay">
+		<div class="corner-icons">
+				<div class="circle-icon">5<b style="color: yellow;">&#9733</b></div>
+  			</div>
           	<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/logo.jpg" alt="Slider Image">
+			<div class="corner-container">
+    			<div class="circle-container"><b style="color: yellow;">$</b> 450</div>
+  			</div>
 			<div class="m-slider-text">
 				<div class="column">
 					<p class="bold-text">RAFTING</p>
@@ -71,7 +80,13 @@
 	<li>
       <div class="m-slider-item">
         <div class="m-slider-overlay">
+		<div class="corner-icons">
+				<div class="circle-icon">5<b style="color: yellow;">&#9733</b></div>
+  			</div>
           	<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/logo.jpg" alt="Slider Image">
+			<div class="corner-container">
+    			<div class="circle-container"><b style="color: yellow;">$</b> 450</div>
+  			</div>
 			<div class="m-slider-text">
 				<div class="column">
 					<p class="bold-text">BIKE RIDES</p>
@@ -90,7 +105,13 @@
 	<li>
       <div class="m-slider-item">
         <div class="m-slider-overlay">
+		<div class="corner-icons">
+				<div class="circle-icon">5<b style="color: yellow;">&#9733</b></div>
+  			</div>
           	<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/logo.jpg" alt="Slider Image">
+			<div class="corner-container">
+    			<div class="circle-container"><b style="color: yellow;">$</b> 450</div>
+  			</div>
 			<div class="m-slider-text">
 				<div class="column">
 					<p class="bold-text">QUAD BIKE RIDES</p>
@@ -101,6 +122,116 @@
 					<p class="gray-text">Person: <b style="color:black;"> 2</b></p>
 					<p class="gray-text">Max speed:</p>
 					<p style="font-size: 20px">80-90 <b style="color: red; font-size: 15px;">km/h</b></p>
+				</div>
+			</div>
+        </div>
+      </div>
+    </li>
+    <!-- Agrega más slides para mostrar 4 elementos a la vez -->
+  </ul>
+</div>
+<div class="l-container">
+  <div class="l-section">
+    <div class="m-wild-trips">
+      <div class="m-text">
+        HOTELS
+      </div>
+      <button class="m-see-all--selected">SEE ALL</button>
+    </div>
+    <div class="m-navigation-controls">
+      <div class="flex-prev m-arrow-icon">
+        <i class=" fa fa-arrow-left"></i>
+      </div>
+      <div class="flex-next m-arrow-icon m-arrow-icon--right">
+        <i class="fa fa-arrow-right"></i>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="flexslider">
+  <ul class="slides">
+    <li>
+      <div class="m-slider-item">
+        <div class="m-slider-overlay">
+		<div class="corner-icons">
+				<div class="circle-icon">5<b style="color: yellow;">&#9733</b></div>
+  			</div>
+          	<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/logo.jpg" alt="Slider Image">
+			<div class="corner-container">
+    			<div class="circle-container"><b style="color: yellow;">$</b> 450</div>
+  			</div>
+			<div class="corner-container">
+    			<div class="circle-container"><b style="color: yellow;">$</b> 450</div>
+  			</div>
+			<div class="m-slider-text">
+				<div class="column">
+					<p class="bold-text">CANCUN HOTEL</p>
+					<p class="gray-text">Lorem Ipsum is simply dummy text of the printing and ...</p>
+					<p class="gray-text"><i class="red-text fa-solid fa-location-dot"></i> Turkey, Mamaria</p>
+					<p class="gray-text"><i class="red-text fa-solid fa-hotel"></i> Rooms available: <b>375</b></p>
+				</div>
+			</div>
+        </div>
+      </div>
+    </li>
+	<li>
+      <div class="m-slider-item">
+        <div class="m-slider-overlay">
+		<div class="corner-icons">
+				<div class="circle-icon">5<b style="color: yellow;">&#9733</b></div>
+  			</div>
+          	<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/logo.jpg" alt="Slider Image">
+			<div class="corner-container">
+    			<div class="circle-container"><b style="color: yellow;">$</b> 450</div>
+  			</div>
+			<div class="m-slider-text">
+			<div class="column">
+					<p class="bold-text">BARCELO COSTA CANCUN</p>
+					<p class="gray-text">Lorem Ipsum is simply dummy text of the printing and ...</p>
+					<p class="gray-text"><i class="red-text fa-solid fa-location-dot"></i> Turkey, Mamaria</p>
+					<p class="gray-text"><i class="red-text fa-solid fa-hotel"></i> Rooms available: <b>375</b></p>
+				</div>
+			</div>
+        </div>
+      </div>
+    </li>
+	<li>
+      <div class="m-slider-item">
+        <div class="m-slider-overlay">
+		<div class="corner-icons">
+				<div class="circle-icon">5<b style="color: yellow;">&#9733</b></div>
+  			</div>
+          	<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/logo.jpg" alt="Slider Image">
+			<div class="corner-container">
+    			<div class="circle-container"><b style="color: yellow;">$</b> 450</div>
+  			</div>
+			<div class="m-slider-text">
+			<div class="column">
+					<p class="bold-text">INDIAN HOTEL</p>
+					<p class="gray-text">Lorem Ipsum is simply dummy text of the printing and ...</p>
+					<p class="gray-text"><i class="red-text fa-solid fa-location-dot"></i> Turkey, Mamaria</p>
+					<p class="gray-text"><i class="red-text fa-solid fa-hotel"></i> Rooms available: <b>375</b></p>
+			</div>
+			</div>
+        </div>
+      </div>
+    </li>
+	<li>
+      <div class="m-slider-item">
+        <div class="m-slider-overlay">
+		<div class="corner-icons">
+				<div class="circle-icon">5<b style="color: yellow;">&#9733</b></div>
+  			</div>
+          	<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/logo.jpg" alt="Slider Image">
+			<div class="corner-container">
+    			<div class="circle-container"><b style="color: yellow;">$</b> 450</div>
+  			</div>
+			<div class="m-slider-text">
+			<div class="column">
+					<p class="bold-text">ART DECO LUXURY HOTEL</p>
+					<p class="gray-text">Lorem Ipsum is simply dummy text of the printing and ...</p>
+					<p class="gray-text"><i class="red-text fa-solid fa-location-dot"></i> Turkey, Mamaria</p>
+					<p class="gray-text"><i class="red-text fa-solid fa-hotel"></i> Rooms available: <b>375</b></p>
 				</div>
 			</div>
         </div>
